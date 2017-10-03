6e5231c80c4275d5a6f8c6adcff74801422cf6a6
JemimaEllis <jemimaeellis@gmail.com>
2017-10-03 13:29:59 +0100

    more changes

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -13,14 +13,26 @@ class CountriesController < ApplicationController
 
   def create
     @countries = Countries.new(countries_params)
+    new_countries.save
+    redirect_to "/countries"
   end
 
   def new
+    @countries = Countries.new
   end
 
   def update
+    countries = Countries.find(params[:id])
+    countries.update = {country: "form-control"}
+    redirect_to "/countries"
   end
 
   def destroy
+    @countries = Countries.destroy(params[:id])
+    redirect_to "countries"
   end
+
+
+
+
 end
